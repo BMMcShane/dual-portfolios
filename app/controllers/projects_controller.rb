2028ea28243cce05f860c1ee79149d7073d73a1a
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-25 10:51:17 -0600

    fixed project adding state update

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,8 +3,14 @@ class ProjectsController < ApplicationController
     def create
         project = Project.create!(user_id: params[:user_id], title: params[:title], description: params[:description], timeframe: params[:timeframe], route: params[:route])
         Award.create(name: params[:awards], project_id: project.id)
-        ProjectLanguage.create(project_id: project.id, language_id: Language.find_by(name: params[:languages]).id)
-        render json: project, status: :created
+        lang = nil
+        if Language.find_by(name: params[:languages])
+            lang = Language.find_by(name: params[:languages])
+        else
+            lang = Language.create(name: params[:languages])
+        end
+        ProjectLanguage.create(project_id: project.id, language_id: lang.id)
+        render json: User.find(params[:user_id]), status: :created
     end
 
     private
